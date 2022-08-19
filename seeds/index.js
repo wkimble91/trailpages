6ade1d03137120760bb5554f23bd988ea3bf101d
Will <wkimble91@gmail.com>
2022-08-19 13:47:45 -0500

    add mongoURL to seed script

--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,8 @@ const cities = require('./cities');
 const { places, descriptors, descriptions, imageURLs } = require('./trails');
 const Trail = require('../models/trails');
 
-mongoose.connect('mongodb://localhost:27017/trail-pages');
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/trail-pages';
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
