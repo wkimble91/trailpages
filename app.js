4646c423c589f461843988d62f56da44418e4277
Will <wkimble91@gmail.com>
2023-02-14 15:12:26 -0600

    removed dev dotenv requirement

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
